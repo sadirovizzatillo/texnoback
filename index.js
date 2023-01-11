cf80796173ad479f916ef2fc65a6f876006435be
sadirov4647 <sadirovizzatillo3@gmail.com>
2023-01-11 18:12:39 +0500

    v1

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.use("/api/purchase", purchase);
 
 
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 8000;
 
 app.listen(port, () => {
     console.log(`${port} portga ulandi`)
